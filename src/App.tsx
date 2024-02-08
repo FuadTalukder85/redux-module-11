14ee89202644471efa19a8429eba81d47d56f92b
Fuad Talukder <webfuadtk@gmail.com>
2024-02-08 18:23:25 +0600

    complete module 11

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import {
+  decrement,
+  increment,
+  incrementByValue,
+} from "./redux/features/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
+
 function App() {
+  const { count } = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <div className="flex border border-purple-300 rounded-md bg-slate-50 p-10">
-        <button className="px-3 py-2 rounded-md bg-green-500 text-xt font-semibold text-white">
+        <button
+          onClick={() => dispatch(incrementByValue(5))}
+          className="px-3 py-2 rounded-md bg-green-500 text-xt font-semibold text-white"
+        >
+          Increment by value 5
+        </button>
+        <button
+          onClick={() => dispatch(increment())}
+          className="px-3 py-2 rounded-md bg-green-500 text-xt font-semibold text-white"
+        >
           Increment
         </button>
-        <h1 className="text-3xl mx-10">0</h1>
-        <button className="px-3 py-2 rounded-md bg-red-500 text-xt font-semibold text-white">
+        <h1 className="text-3xl mx-10">{count}</h1>
+        <button
+          onClick={() => dispatch(decrement())}
+          className="px-3 py-2 rounded-md bg-red-500 text-xt font-semibold text-white"
+        >
           Increment
         </button>
       </div>
